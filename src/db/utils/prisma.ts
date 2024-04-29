a09549afc4d82cbafc6714f8fd81ea4dd504265b
Rizky Alam <sketsacartoon@gmail.com>
2024-04-29 14:49:16 +0700

    feat: update prisma singleton

--- src/db/utils/prisma.ts
+++ src/db/utils/prisma.ts
@@ -1,2 +1,18 @@
+// import { PrismaClient } from "@prisma/client";
+// export const prisma = new PrismaClient();
+
 import { PrismaClient } from "@prisma/client";
-export const prisma = new PrismaClient();
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  const prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
